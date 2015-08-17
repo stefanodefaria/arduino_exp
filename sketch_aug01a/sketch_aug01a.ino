ef0bc9d6b4fe78e4e7944f1951a3d74ad23dfed5
stefano <stefanodefaria@gmail.com>
2015-08-16 21:12:55 -0300

    - Adicionei constantes para valores de mensagens - Deixei o codigo mais assincrono com delays menores

--- sketch_aug01a/sketch_aug01a.ino
+++ sketch_aug01a/sketch_aug01a.ino
@@ -1,15 +1,25 @@
-char ledArduino = 13;
-char ledVermelho = 9;
+const char ledArduino = 13;
+const char ledVermelho = 9;
+
+const unsigned int ledInitialCount = 400;
+
+const char AVAILABLE = '0';
+const char BUSY = '1';
+const char END = '2';
+const char START = '3';
+const char REQUEST_STATUS = '4';
+const char RESET = '5';
+const char SUCCESS = '6';
+const char UNKNOWN_COMMAND = '7';
 
 boolean ledArduinoAceso = false;
 boolean ledVermelhoAceso = false;
 
-boolean start = false;
-boolean finished = true;
+boolean busy = false;
 
 int countArduino = 0;   //repeticoes, deve ser par
 int countVermelho = 0;  //repeticoes, deve ser par
-int delayTime = 500;     //ms
+int delayTime = 10;     //ms
 
 
 void setup() {
@@ -19,50 +29,69 @@ void setup() {
 }
 
 void initialize(){
-  countArduino = 8;   //repeticoes, deve ser par
-  countVermelho = 8;
-  start = true;
-  finished = false;
+  countArduino = ledInitialCount;   //repeticoes, deve ser par
+  countVermelho = ledInitialCount;  //repeticoes, deve ser par
+  digitalWrite(ledArduino, LOW);
+  digitalWrite(ledVermelho, LOW);
+  ledVermelhoAceso = false;
+  ledArduinoAceso = false;
+  busy = false;
 }
 
 void serialEvent(){
   char comando = Serial.read();
   
   switch (comando){
-    case '1':
-      initialize();
-      Serial.write("0");
-      break;
-    case '2':
-      if(finished){
-        Serial.write("3");
-      }
-      else if (start){
-        Serial.write("1");
+    case START:
+      if(busy){
+        Serial.write(BUSY);
       }
       else{
-        Serial.write("4");
+        initialize();
+        busy = true;
+        Serial.write(SUCCESS);
       }
+      break;
+      
+    case REQUEST_STATUS:
+      if(busy){
+        Serial.write(BUSY);
+      }
+      else{
+        Serial.write(AVAILABLE);
+      }
+      break;
+    
+    case RESET:      
+      initialize();
+      Serial.write(SUCCESS);
+      break;
+    
+    default:
+      Serial.write(UNKNOWN_COMMAND);
       break;
   }
 }
 
 void loop()
 {    
-  if(countArduino && start && !finished){
-    ledArduinoAceso = toggleLed(ledArduino, ledArduinoAceso);
-    countArduino--;
-  }
-  else if(countVermelho && start && !finished){
-    ledVermelhoAceso = toggleLed(ledVermelho, ledVermelhoAceso);
-    countVermelho--;
-  }
-  else if(start && !finished){
-    finished = true;
-    start = false;
-    Serial.write("2");
-  }
-  
+  if(busy){
+    if(countArduino){
+      if(!(countArduino % 50))
+        ledArduinoAceso = toggleLed(ledArduino, ledArduinoAceso);
+      countArduino--;
+    }
+    if(!countArduino && countVermelho){
+      if(!(countVermelho % 50))
+        ledVermelhoAceso = toggleLed(ledVermelho, ledVermelhoAceso);
+      countVermelho--;
+    }
+    if(!countArduino && !countVermelho){      
+      Serial.write(END);
+      busy = false;
+    }
+  }  
+  delay(delayTime);  
 }
 
 boolean toggleLed(int led, boolean aceso){
@@ -72,6 +101,5 @@ boolean toggleLed(int led, boolean aceso){
   else{
     digitalWrite(led, HIGH);
   }  
-  delay(delayTime);
   return !aceso;
 }
